6410fd2776b80c6eb7c1e256b2e44a05c9c432a3
Chamie <mail@chamie.fr>
2018-12-05 13:31:37 +0100

    Update for 2019 -2020 season
    1192 refere to adhesion 2020 product

--- wc-auto-add-renew-membership.php
+++ wc-auto-add-renew-membership.php
@@ -21,7 +21,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package		Auto Add or Renew Membership for WooCommerce
  * @author		Maria Gorska
@@ -29,11 +29,9 @@
  */
 
 function add_membership_on_adding_product_to_cart ( $cart_key, $product_id, $quantity ) {
-	$membership_product_id = 226; 
-	$current_plan_slug = 'adhesion-2018';
+	$membership_product_id = 291; 
+	$current_plan_slug = 'adhesion-2019';
 	$found = false;
-	// Chamie
-	$oldPlanSlug = 'adherent-2018';
 	
 	// bail if Memberships isn't active
 	if ( ! function_exists( 'wc_memberships' ) ) {
@@ -42,8 +40,7 @@ function add_membership_on_adding_product_to_cart ( $cart_key, $product_id, $qua
 	
 	$user_id = get_current_user_id();
 	// If the user is not yet a member, add the membership product to cart, unless it's already there
-	if ( ! wc_memberships_is_user_active_member( $user_id, $current_plan_slug ) ||
-		! wc_memberships_is_user_active_member( $user_id, $oldPlanSlug ) ) {
+	if ( ! wc_memberships_is_user_active_member( $user_id, $current_plan_slug ) ) {
 	//check if product already in cart
 		if ( sizeof( WC()->cart->get_cart() ) > 0 ) {
 			foreach ( WC()->cart->get_cart() as $cart_item_key => $values ) {
@@ -65,7 +62,7 @@ function add_membership_on_adding_product_to_cart ( $cart_key, $product_id, $qua
 add_action( 'woocommerce_add_to_cart', 'add_membership_on_adding_product_to_cart', 10, 3 );
 
 function maybe_add_next_year_membership_to_cart ( $user_id, $product_id, $current_plan_slug ) {
-	$next_year_product_id = 291; 
+	$next_year_product_id = 1192; 
 	
 	$months_to_expire = 0;
 	
@@ -93,7 +90,7 @@ function maybe_add_next_year_membership_to_cart ( $user_id, $product_id, $curren
 	if ( $expiry_year > $current_year ) {
 		WC()->cart->add_to_cart( $next_year_product_id );	
 		if ( wc_memberships_is_user_active_member( $user_id, $current_plan_slug ) ) {
-			wc_add_notice( sprintf( __( 'Your yearly membership expires before the end of the current billing period. The membership for new year has been added to cart automatically.', 'wc-auto-add-renew-membership' ), wc_get_page_permalink( 'myaccount' ) ), 'notice' );		
+			wc_add_notice( sprintf( __( 'Your yearly membership expires before the end of the current billing period. The membership for new year has been added to cart automatically.', 'wc-auto-add-renew-membership' ), wc_get_page_permalink( 'myaccount' ) ), 'notice' );
 		}
 	}
 
